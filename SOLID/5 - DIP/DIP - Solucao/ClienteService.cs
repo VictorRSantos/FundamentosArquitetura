ec94bfb50e94a63b90b25996c9d659fc46c1f03b
Victor R Santos <vrsisa@hotmail.com>
2024-04-22 08:58:32 -0300

    [estudos_solid]Implementando DIP - Dependency Inversion Principle

---
+++ SOLID/5 - DIP/DIP - Solucao/ClienteService.cs
@@ -0,0 +1,28 @@
+﻿using SOLID.DIP.Solucao.Interfaces;
+
+namespace SOLID.DIP.Solucao
+{
+    public class ClienteService : IClienteServices
+    {
+        private readonly IClienteRepository _clienteRepository;
+        private readonly IEmailServices _emailServices;
+
+        public ClienteService(IEmailServices emailServices, IClienteRepository clienteRepository)
+        {
+            _emailServices = emailServices;
+            _clienteRepository = clienteRepository;
+        }
+
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+                return "Dados inválidos";
+
+            _clienteRepository.AdicionarCliente(cliente);
+
+            _emailServices.Enviar(de: "empresa@empresa.com", para: cliente.Email.Endereco, assunto: "Bem Vindo", mensagem: "Parabéns! você está cadastrado");
+
+            return "Cliente cadastrado com sucesso!";
+        }
+    }
+}
---
+++ SOLID/5 - DIP/DIP - Violacao/ClienteService.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID.DIP.Violacao
+{
+    public class ClienteService
+    {
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+                return "Dados inválidos";
+
+            var repo = new ClienteRepository();
+            repo.AdionarCliente(cliente);
+
+            EmailService.Enviar(de: "empresa@empresa.com", para: cliente.Email.Endereco, assunto: "Bem Vindo", mensagem: "Parabéns! você está cadastrado");
+
+            return "Cliente cadastrado com sucesso!";
+        }
+    }
+}
